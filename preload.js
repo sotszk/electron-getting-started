214a62d68a231df79c3ddc2a182b487606747f8d
SotaSuzuki <3.3sota.suzuki@gmail.com>
2023-03-01 19:08:54 +0900

    add preload.js and renderer.js

---
+++ preload.js
@@ -0,0 +1,15 @@
+/*
+preload.js
+- renderer が web page を読み込む前に実行されるスクリプト
+- preload script は HTML, DOM, および Node.js と Electron API のサブセットにアクセス可能なコンテキストで実行される
+  - Electron API （モジュール）: レンダラプロセスのモジュールにアクセス可能
+  - Node.js: events, url, timers
+*/
+
+const { contextBridge } = require("electron");
+
+contextBridge.exposeInMainWorld("versions", {
+  node: () => process.version.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+});
---
+++ renderer.js
@@ -0,0 +1,4 @@
+const infoEl = document.getElementById("info");
+
+// preload.js によって global(window) に versions が expose されているので下記のように versions にアクセス可能
+infoEl.innerHTML = `This app is using Chrome (v${window.versions.chrome()})`;

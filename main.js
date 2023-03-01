4691d6a0c0e73556a20063f1a22c6e7ad397b074
SotaSuzuki <3.3sota.suzuki@gmail.com>
2023-03-01 18:41:29 +0900

    refactor

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const { app, BrowserWindow } = require('electron');
 
-app.whenReady().then(() => {
+const createWindow = () => {
   const aWindow = new BrowserWindow({
     width: 800,
     height: 600,
   });
 
   aWindow.loadFile('./index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
 })

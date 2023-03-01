1fa62d365ab00dfbea5b341757b8b21295795c4d
SotaSuzuki <3.3sota.suzuki@gmail.com>
2023-03-01 17:21:38 +0900

    add loading html

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("Hello Electron!");
+const { app, BrowserWindow } = require('electron');
+
+app.whenReady().then(() => {
+  const aWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  aWindow.loadFile('./index.html');
+})
